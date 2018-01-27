5550ecacc0636e7cdda9307d3a3fa490a376e7a5
Sergey Melnychuk <sergiomelnychuk@gmail.com>
2018-01-27 16:25:26 +0200

    add new project

---
+++ Redux-practice/src/app.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export default class App extends Comment {
+    render (){
+        return (
+          <div>
+              Hello!!
+          </div>
+        )
+    }
+}
---
+++ Redux-practice/webpack.config.js
@@ -0,0 +1,62 @@
+const webpack = require('webpack')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const outputPath = path.resolve(__dirname, './dist')
+
+const webpackConfig = {
+	entry: {
+		app: [
+			path.resolve(__dirname, './src/index.js')
+		]
+	},
+	output: {
+		path: outputPath,
+		filename: '[name].js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: 'babel-loader'
+			},
+			{
+				test: /\.scss$/,
+				exclude: /node_modules/,
+				use: [
+					'style-loader',
+					'css-loader',
+					'sass-loader'
+				]
+			},
+			{
+				test: /\.(gif|png|jpg|jpeg|svg)$/,
+				exclude: /node_modules/,
+				include: path.resolve(__dirname, './src/assets/'),
+				use: 'url-loader?limit=10000&name=assets/[name]-[hash].[ext]'
+			}
+		]
+	},
+	plugins: [
+        //adds bundle scripts src into the buttom of the page
+		new HtmlWebpackPlugin({
+			template: path.join(__dirname, './src/assets/index.html'),
+			filename: 'index.html',
+			path: outputPath
+        }),
+        //will write into console log what file or module was modified
+        new webpack.NamedModulesPlugin(),
+        //refresh content without refresh
+		new webpack.HotModuleReplacementPlugin() 
+	],
+	devServer: {
+		contentBase: path.resolve(__dirname, './dist'),
+		port: 8080,
+		historyApiFallback: true,
+		inline: true,
+		hot: true,
+		host: '0.0.0.0'
+	}
+}
+
+module.exports = webpackConfig 
