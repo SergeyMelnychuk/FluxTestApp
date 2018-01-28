be827f37243fd0e94497b81e8923899c05a5be6e
Sergey Melnychuk <sergiomelnychuk@gmail.com>
2018-01-28 16:46:13 +0200

    clean up

--- setup-env/src/containers/App.js
+++ setup-env/src/containers/App.js
@@ -6,7 +6,7 @@ import React, { Component } from 'react'
 export default class App extends Component {
 	render() {
 		return (				
-			<div>Helloregefdgvsdrg!!!</div>
+			<div>Hello!!!</div>
 		)
 	}
 }
